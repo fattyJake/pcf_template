bc471704c3636bc76152ca81c463139cbe18891a
Jacob <jacobwang123@gmail.com>
2020-01-21 09:43:55 -0500

    pep8 complaint

--- app.py
+++ app.py
@@ -6,19 +6,16 @@ from flask_swagger_ui import get_swaggerui_blueprint
 application = Flask(__name__)
 
 ### swagger specific ###
-SWAGGER_URL = '/documentation'
-API_URL = '/static/openapi.json'
+SWAGGER_URL = "/documentation"
+API_URL = "/static/openapi.json"
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "CARAMLService"
-    }
+    SWAGGER_URL, API_URL, config={"app_name": "CARAMLService"}
 )
 application.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 ### end swagger specific ###
 
-@application.route('/', methods=['GET', 'POST'])
+
+@application.route("/", methods=["GET", "POST"])
 def welcome():
     response_text = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
         <title>CARA ML Service</title>
@@ -26,21 +23,23 @@ def welcome():
         <p></p>"""
     return response_text
 
-@application.route('/do_stuff', methods=['POST'])
+
+@application.route("/do_stuff", methods=["POST"])
 def do_stuff():
     content = request.get_json(silent=True)
     try:
         # do something
         return jsonify(content), 200
     except Exception as e:
-        tb = traceback.format_exc().split('\n')
+        tb = traceback.format_exc().split("\n")
         error_response = {
             "Request Status": "Failed",
             "Error": e.__class__.__name__,
             "Error Message": str(e),
-            "stack_trace": "\n".join(tb)
+            "stack_trace": "\n".join(tb),
         }
         return jsonify(error_response), 500
 
+
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host="0.0.0.0")
